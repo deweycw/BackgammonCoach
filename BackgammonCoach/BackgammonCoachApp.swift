fee01d2a1d733175eca856c29447484e47356843
Christian Dewey <dewey.christian@gmail.com>
2026-02-08 13:11:56 -0500

    Initial Commit

---
+++ BackgammonCoach/BackgammonCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgammonCoachApp.swift
+//  BackgammonCoach
+//
+//  Created by Christian Dewey on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BackgammonCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
